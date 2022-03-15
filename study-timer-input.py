e6e2b0c1a13d34aec43c305b9ec428e6eb81f667
gusBrasil <gustavomedbra@gmail.com>
2022-03-15 18:54:18 -0300

    input

---
+++ study-timer-input.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+
+# timer is set for 10 minutes after activated
+for i in range(1):
+    time.sleep(float(input("How long do you want to wait? (in seconds) ")))
+
+# just pick any cool playlist you want and have fun :) 
+    webbrowser.open("https://www.youtube.com/watch?v=f02mOEt11OQ")
